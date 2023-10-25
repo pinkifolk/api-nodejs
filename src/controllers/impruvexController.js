a361ddc8a2c16880bd277a6225628aa90be32cbb
pinkifolk <ssoliswebmaster@gmail.com>
2023-10-25 14:08:08 -0300

    add table and insert data

--- src/controllers/impruvexController.js
+++ src/controllers/impruvexController.js
@@ -1,20 +1,22 @@
 export const verificarAsn = async (req, res) => {
     const post = req.body
-    const subPost = req.body.listaCajas // get nested 
-    const insert = [
-        [post.idasn, post.unenviadas, post.unrecibidas, post.fecharecep, post.fechacreacion, subPost[0].sku, subPost[0].unidades]
-    ]
+    const subPost = req.body.listaCajas // get nested
+
     await req.getConnection(async (error, conexion) => {
         if (error) return res.send(error)
-        // await conexion.query('INSERT INTO nameTable VALUES ?;', insert, (err, data) => {
-        //     if (err) return console.log(err)
-        // })
-        res.json({
-            "data": true,
-            "messaje": "Datos Recibidos",
-            "results": insert
-
+        subPost.forEach(e => {
+            const sql = 'INSERT INTO recepciones_invas VALUES (?,?,?,?,?,?);'
+            conexion.query(sql, [, post.fecharecep, post.idasn, e.sku, e.unidades, 0], (err, results) => {
+                if (err) return console.log(err)
+                res.json({
+                    "data": true,
+                    "messaje": "Datos Recibidos",
+                    "columnasAfectadas": results.affectedRows,
+                    "alertas": results.warningCount,
+                })
+            })
         })
+
     })
 }
 export const despachoExpress = async (req, res) => {
